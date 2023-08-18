41acf94441636a1dceff503ebea06614039a1b9f
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-08-18 15:59:53 -0600

    commit tree 1a69aa4bf283d8cdbabcc03755c8f56b65d0d8f7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = 
+    if params[:first_name] 
+      Student.find_by(first_name: params[:first_name])
+    elsif params[:last_name] 
+        Student.find_by(last_name: params[:last_name])
+    else
+      Student.all
+    end
     render json: students
   end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
 
 end
